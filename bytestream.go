4664a7d3ca2b8a8780a19523193a50f707592c39
Jackson Massey <jackmassey2000@gmail.com>
2023-12-03 15:50:41 -0600

    implementing stream list and html templating

--- bytestream.go
+++ bytestream.go
@@ -1,49 +1,107 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"net/http"
+	"regexp"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 type stream struct {
-	StreamURL      string `json:"streamURL"`
-	StreamName     string `json:"streamname"`
-	StreamReferrer string `json:"streamreferrer"`
+	StreamURL      string    `json:"streamurl"`
+	StreamName     string    `json:"streamname"`
+	StreamReferrer string    `json:"streamreferrer"`
+	AddedTime      time.Time `json:"-"` //JSON ignores this field
 }
 
 var SINGLE_STREAM stream
 
-func addStream(c *gin.Context) {
+var STREAMS = []stream{}
 
-	if err := c.BindJSON(&s); err != nil {
-		return
+func validateStream(s stream) error {
+	urlRegex := `^(http(s):\/\/.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$`
+	fmt.Println(s.StreamURL)
+	fmt.Println(s.StreamReferrer)
+	fmt.Println(s.StreamName)
+	if s.StreamURL == "" || s.StreamReferrer == "" {
+		return errors.New("Stream URL or Referrer is empty")
 	}
 
-	c.IndentedJSON(http.StatusCreated, newAlbum)
+	match, err := regexp.MatchString(urlRegex, s.StreamURL)
+	if err != nil {
+		return err
+	}
+	if !match {
+		return errors.New("Invalid Stream URL, must be a valid URL starting with http(s)://")
+	}
+
+	return nil
+}
+
+func addStream(c *gin.Context) {
+	new_stream := stream{}
+	new_stream.AddedTime = time.Now()
+	if err := c.BindJSON(&new_stream); err != nil {
+		c.Status(http.StatusBadRequest)
+		fmt.Println(err)
+		return
+	}
+	if err := validateStream(new_stream); err != nil {
+		c.Status(http.StatusBadRequest)
+		fmt.Println(err)
+		return
+	}
+	STREAMS = append(STREAMS, new_stream) //Add the stream to the list
+	c.Status(http.StatusCreated)
 }
 
 func getIndex(c *gin.Context) {
-	c.HTML(200, "index.html", gin.H{
+	c.HTML(200, "index.tmpl", gin.H{
 		"title": "bytestream - home",
 	})
 }
 
-func getUFC(c *gin.Context) {
-	c.HTML(200, "hls.html", gin.H{
-		"title": "bytestream - ufc",
+func getPlayer(c *gin.Context) {
+	c.HTML(200, "player.tmpl", gin.H{
+		"title": "bytestream - player",
 	})
 }
 
+func instantiateStreamList() {
+	s1 := stream{
+		StreamURL:      "https://www.youtube.com/watch?v=9E9hHkyZ8Lg",
+		StreamName:     "UFC 257",
+		StreamReferrer: "https://www.youtube.com/",
+		AddedTime:      time.Now(),
+	}
+	s2 := stream{
+		StreamURL:      "https://www.youtube.com/watch?v=9E9hHkyZ8Lg",
+		StreamName:     "UFC 257",
+		StreamReferrer: "https://www.youtube.com/",
+		AddedTime:      time.Now(),
+	}
+	s3 := stream{
+		StreamURL:      "https://www.youtube.com/watch?v=9E9hHkyZ8Lg",
+		StreamName:     "UFC 257",
+		StreamReferrer: "https://www.youtube.com/",
+		AddedTime:      time.Now(),
+	}
+	STREAMS = append(STREAMS, s1, s2, s3)
+}
+
 func main() {
+	instantiateStreamList() //TODO remove
 	r := gin.Default()
 
 	r.Static("/hls", "./hls")
-	r.Static("/public", "./public")
 	r.LoadHTMLGlob("web/*.html")
 
 	r.GET("/", getIndex)
-	r.GET("/stream", getUFC)
+	r.GET("/player", getPlayer)
+	r.POST("/addstream", addStream)
 
 	r.Run(":8081") // listen and serve on 0.0.0.0:8081
 }
---
+++ scripts/post_addstream.sh
@@ -0,0 +1,3 @@
+curl -X POST http://127.0.0.1:8081/addstream  \
+     -H 'Content-Type: application/json' \
+     -d '{"streamurl":"https://ed-c003.edgking.me/plyvivo/cipotimu1aluximet0xi/chunklist.m3u8","streamname":"UFC Fight Night","streamreferrer":"https://www.niaomea.me"}'
